a9dcfe4849c8dd7fa1758e8d0c18a4bd4ee3363b
Filipe Zabala <filipezabala@MacBook-Air-de-Filipe.local>
2020-10-09 18:49:27 -0300

    devtools::document soved

---
+++ testthat/main.R
@@ -0,0 +1,33 @@
+# main file
+# http://r-pkgs.had.co.nz/
+
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+# packs
+library(devtools)
+
+# session_info
+session_info()
+
+# updating and creating manual
+setwd('~/Dropbox/PUC/GEARS/ffit/')
+devtools::document(setwd('~/Dropbox/PUC/GEARS/ffit/'))
+
+# loading
+devtools::load_all()
+
+# installing
+update.packages(ask=F)
+devtools::install_github('filipezabala/ffit', force = T)
+
+# attaching
+library(voice)
+?conv_df
+?conv_mc
+?extract_features
+find.package('voice')
+packageDescription('voice')
